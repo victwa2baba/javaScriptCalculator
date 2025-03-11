4a182301cd128242fccbbcff9d4ba08b090f48b0
videv <vtounaregha@gmail.com>
2025-03-11 15:03:02 +0100

    Added the basic html, buttons with bootstrap

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+let add = (a, b) => a + b;
+let substract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+let operate = (a, op, b) => {
+    if (op === '+') {
+        return add(a, b);
+    } else if (op === '-') {
+        return substract(a, b);
+    } else if (op === '*') {
+        return multiply(a, b);
+    } else if (op === '/') {
+        return divide(a, b);
+    }
+}
